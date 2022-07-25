8e1e8a10b94dba9d39557c3939ee989402d2a843
Joel Lord <joelphy@gmail.com>
2022-07-25 13:27:26 -0500

    Base template

---
+++ front/src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return(
+    <div className="title">
+      Welcome to my book collection
+    </div>
+  )
+}
---
+++ front/src/pages/Books.js
@@ -0,0 +1,11 @@
+export default function Books() {
+  return(
+    <div>
+      Here is a list of books I own:
+      <ul>
+        <li>ALICE IN WONDERLAND <button>Show Author</button></li>
+        <li>BILBO THE HOBBIT <button>Show Author</button></li>
+      </ul>
+    </div>
+  )
+}
